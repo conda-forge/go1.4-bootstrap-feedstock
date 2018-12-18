652777295d6d166ecfafc28061d72e7d44183d14
Patrick Sodré <psodre@gmail.com>
2018-12-18 01:57:07 +0000

    fix typo

--- recipe/build-core.bat
+++ recipe/build-core.bat
@@ -7,7 +7,7 @@ call make.bat
 if errorlevel 1 exit 1
 
 rem Don't need the cached build objects
-rmdir /s /q %SRC_DIR%\go1.4-bootstrap\pkg\obj
+rmdir /s /q "%SRC_DIR%\go1.4-bootstrap\pkg\obj"
 
 mkdir "%PREFIX%\go1.4-bootstrap"
 xcopy /s /y /i /q "%SRC_DIR%\go1.4-bootstrap\*" "%PREFIX%\go1.4-bootstrap\"
@@ -17,7 +17,7 @@ rem go finds its *.go files via the GOROOT variable
 for %%F in (activate deactivate) do (
   if not exist "%PREFIX%\etc\conda\%%F.d" mkdir "%PREFIX%\etc\conda\%%F.d"
   if errorlevel 1 exit 1
-  copy "%RECIPE_DIR%\%%F-g1.4-bootstrap.bat" "%PREFIX%\etc\conda\%%F.d\go1.4-bootstrap.bat"
+  copy "%RECIPE_DIR%\%%F-go1.4-bootstrap.bat" "%PREFIX%\etc\conda\%%F.d\go1.4-bootstrap.bat"
   if errorlevel 1 exit 1
   dir %PREFIX%\etc\conda\%%F.d\
 )
